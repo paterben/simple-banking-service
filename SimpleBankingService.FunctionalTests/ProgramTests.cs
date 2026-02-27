ce92b76410fe846614043dbf4368ec6924bf2a72
Benjamin Paterson <paterben@hotmail.com>
2026-02-27 20:30:45 +1100

    Add an end-to-end test with failing transactions

--- SimpleBankingService.FunctionalTests/ProgramTests.cs
+++ SimpleBankingService.FunctionalTests/ProgramTests.cs
@@ -17,4 +17,14 @@ public sealed class ProgramTests
 
         fileContents.Should().Be(expectedFileContents);
     }
+
+    [TestMethod]
+    public async Task DoRootCommandAsync_WhenFailingTransactions_Throws()
+    {
+        var action = async () =>
+          await Program.DoRootCommandAsync(new FileInfo(@"data\account_balances.csv"), new FileInfo(@"data\failing_transactions.csv"), new FileInfo(@"data\output.csv"));
+        await action
+          .Should().ThrowAsync<InvalidOperationException>()
+          .WithMessage("ApplyToBalance: account 1111234522226789 has insufficient balance to subtract 5000.00: current balance 4500.00");
+    }
 }
